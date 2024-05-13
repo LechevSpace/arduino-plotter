745577cc2b12dc47b84857dec84eabc0a85c46e2
Lachezar Lechev <elpiel93@gmail.com>
2024-05-13 11:35:58 +0300

    chore: lib.rs crate attributes
    Signed-off-by: Lachezar Lechev <elpiel93@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![deny(clippy::all)]
+#![deny(rustdoc::broken_intra_doc_links)]
+// only enables the `doc_cfg` feature when
+// the `docsrs` configuration attribute is defined
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 #[doc(inline)]
 pub use api::*;
 
